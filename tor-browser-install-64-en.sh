122a5e56557f6bf6ecf700c637c9863f2a8af9c6
O7zSO31IVg <98870352+O7zSO31IVg@users.noreply.github.com>
2022-02-22 12:11:11 +0700

    Update tor-browser-install-64-en.sh

--- tor-browser-install-64-en.sh
+++ tor-browser-install-64-en.sh
@@ -4,13 +4,13 @@
 wget https://www.torproject.org/dist/torbrowser/11.0.6/tor-browser-linux64-11.0.6_en-US.tar.xz
 
 # Extract
-tar -xf tor-browser-linux64-*_en-US.tar.xz
+tar -xf tor-browser-linux64-11.0.6_en-US.tar.xz
 
 # Delete Tor Browser package to free space
-rm tor-browser-linux64-*_en-US.tar.xz
+rm tor-browser-linux64-11.0.6_en-US.tar.xz
 
 # Move to Tor Browser directory
-cd /Download/tor-browser-linux64-*_en-US
+cd /Download/tor-browser-linux64-11.0.6_en-US
 
 # Make the file executable
 chmod +x start-tor-browser.desktop
