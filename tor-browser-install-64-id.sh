568100cd219cafd2aa906f0e78ca83459a21c2ea
O7zSO31IVg <98870352+O7zSO31IVg@users.noreply.github.com>
2022-02-22 10:44:51 +0700

    Update tor-browser-install-64-id.sh

--- tor-browser-install-64-id.sh
+++ tor-browser-install-64-id.sh
@@ -6,7 +6,7 @@ wget https://www.torproject.org/dist/torbrowser/11.0.6/tor-browser-linux64-11.0.
 # Ekstrak
 tar -xf tor-browser-linux64-11.0.6_id.tar.xz
 
-# Hapus paket Tor Browser untuk membebaskan ruang
+# Hapus paket Tor Browser untuk membebaskan memori
 rm tor-browser-linux64-*_id.tar.xz
 
 # Pindah ke direktori Tor Browser
