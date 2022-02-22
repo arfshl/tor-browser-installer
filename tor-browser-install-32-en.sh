e192cdaf2ce224305a1fa657b2f1d66845ccc261
O7zSO31IVg <98870352+O7zSO31IVg@users.noreply.github.com>
2022-02-22 12:00:32 +0700

    Add files via upload

---
+++ tor-browser-install-32-en.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Installing wget
+sudo apt install wget
+
+# Download Tor Browser package
+wget https://www.torproject.org/dist/torbrowser/11.0.6/tor-browser-linux32-11.0.6_en-US.tar.xz
+
+# Extract
+tar -xf tor-browser-linux32-*_en-US.tar.xz
+
+# Delete Tor Browser package to free space
+rm tor-browser-linux32-*_en-US.tar.xz
+
+# Move to Tor Browser directory
+cd /Download/tor-browser-linux32-*_en-US
+
+# Make the file executable
+chmod +x start-tor-browser.desktop
+
+# Execute + Register as app
+bash start-tor-browser.desktop --register-app
---
+++ tor-browser-install-64-id.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Menginstal wget
+sudo apt install wget
+
+# Unduh paket Tor Browser
+wget https://www.torproject.org/dist/torbrowser/11.0.6/tor-browser-linux64-11.0.6_id.tar.xz
+
+# Ekstrak
+tar -xf tor-browser-linux64-*_id.tar.xz
+
+# Hapus paket Tor Browser untuk membebaskan memori
+rm tor-browser-linux64-*_id.tar.xz
+
+# Pindah ke direktori Tor Browser
+cd /Download/tor-browser-linux64-*_id
+
+# Membuat file dapat dieksekusi
+chmod +x start-tor-browser.desktop
+
+# Eksekusi + Daftarkan sebagai Aplikasi
+bash start-tor-browser.desktop --register-app
