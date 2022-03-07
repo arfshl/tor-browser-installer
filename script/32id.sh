66a7655b470ba6b6ec51266b6669378d822f4559
O7zSO31IVg <98870352+O7zSO31IVg@users.noreply.github.com>
2022-03-07 12:03:19 +0700

    Update 32id.sh

--- script/32id.sh
+++ script/32id.sh
@@ -1,29 +1,17 @@
 #!/bin/sh
 
-echo Mengunduh paket Tor Browser
+echo Menginstal Tor Browser
 
 wget https://www.torproject.org/dist/torbrowser/11.0.6/tor-browser-linux32-11.0.6_id.tar.xz
 
-echo Memindahkan ke direktori download
+cd ~/Download
 
-cd /Download
+tar -xf tor-browser-linux*.tar.xz
 
-echo Mengekstrak
+rm tor-browser-linux*.tar.xz
 
-tar -xf tor-browser-linux32-11.0.6_id.tar.xz
-
-echo menghapus paket Tor Browser untuk membebaskan memori
-
-rm tor-browser-linux32-11.0.6_id.tar.xz
-
-echo Memindahkan ke direktori Tor Browser
-
-cd /Download/tor-browser-linux32-11.0.6_id
-
-echo Membuat file dapat dieksekusi
+cd ~/Download/tor-browser-linux*
 
 chmod +x start-tor-browser.desktop
-
-echo Mengeksekusi + Mendaftarkan sebagai Aplikasi
 
 ./start-tor-browser.desktop --register-app
