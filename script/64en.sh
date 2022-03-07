b30dd5fc307898557db9d3768576332a88dce659
O7zSO31IVg <98870352+O7zSO31IVg@users.noreply.github.com>
2022-03-07 11:49:50 +0700

    Update 64en.sh

--- script/64en.sh
+++ script/64en.sh
@@ -1,29 +1,17 @@
 #!/bin/sh
 
-echo Downloading Tor Browser package
+echo Installing Tor Browser
 
 wget https://www.torproject.org/dist/torbrowser/11.0.6/tor-browser-linux64-11.0.6_en-US.tar.xz
 
-echo Moving to download directory
+cd ~/Download
 
-cd /Download
+tar -xf tor-browser-linux*.tar.xz
 
-echo Extracting
+rm tor-browser-linux*.tar.xz
 
-tar -xf tor-browser-linux64-11.0.6_en-US.tar.xz
-
-echo Deleteming Tor Browser package to free space
-
-rm tor-browser-linux64-11.0.6_en-US.tar.xz
-
-echo Moving to Tor Browser directory
-
-cd /Download/tor-browser-linux64-11.0.6_en-US
-
-echo Make the file executable
+cd /Download/tor-browser-linux*
 
 chmod +x start-tor-browser.desktop
-
-echo Executing + Registering Tor Browser as an app
 
 ./start-tor-browser.desktop --register-app
