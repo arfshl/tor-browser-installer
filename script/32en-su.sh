1e9a6bb5b5bfd94bdfa10f7255f76ce71806bef6
O7zSO31IVg <98870352+O7zSO31IVg@users.noreply.github.com>
2022-03-29 18:54:15 +0700

    Create 32en-su.sh

---
+++ script/32en-su.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+wget https://dist.torproject.org/torbrowser/11.0.9/tor-browser-linux32-11.0.9_en-US.tar.xz -P ~/Download
+
+cd ~/Download
+
+tar -xvf tor-browser-linux32-*_en-US.tar.xz
+
+rm tor-browser-linux32-*_en-US.tar.xz
+
+mv tor-browser-linux32-*_en-US /opt
+
+cd /opt/tor-browser-linux32-*_en-US
+
+chmod +x start-tor-browser.desktop
+
+./start-tor-browser.desktop --register-app
