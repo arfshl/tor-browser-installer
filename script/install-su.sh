466b984ca41b1ee700b54db039d087625f54e1a0
O7zSO31IVg <98870352+O7zSO31IVg@users.noreply.github.com>
2022-04-01 14:50:23 +0700

    Update install-su.sh

--- script/install-su.sh
+++ script/install-su.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-cd ~/Download
-tar -xf tor-browser-linux*.tar.xz
-rm tor-browser-linux*.tar.xz
-mv tor-browser-linux* /opt
+mv tor-browser-linux*.tar.bz2 /opt
+cd /opt
+tar xvjf tor-browser-linux*.tar.bz2
+rm tor-browser-linux*.tar.bz2
 cd /opt/tor-browser-linux*
 chmod +x start-tor-browser.desktop
-./start-tor-browser.desktop --register-app 
+./start-tor-browser.desktop --register-app
