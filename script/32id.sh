6e07337cc90b13e1cd48f0a675b716fdb3871287
O7zSO31IVg <98870352+O7zSO31IVg@users.noreply.github.com>
2022-04-01 07:34:29 +0700

    Update 32id.sh

--- script/32id.sh
+++ script/32id.sh
@@ -1,17 +1,8 @@
 #!/bin/sh
-
-wget https://dist.torproject.org/torbrowser/11.0.9/tor-browser-linux32-11.0.9_id.tar.xz -P ~/Download
-
 cd ~/Download
-
-tar -xf tor-browser-linux32-*_id.tar.xz
-
-rm tor-browser-linux32-*_id.tar.xz
-
-sudo mv tor-browser-linux32-*_id /opt
-
-sudo cd /opt/tor-browser-linux32-*_id
-
+tar -xf tor-browser-linux*.tar.xz
+rm tor-browser-linux*.tar.xz
+sudo mv tor-browser-linux* /opt
+sudo cd /opt/tor-browser-linux*
 sudo chmod +x start-tor-browser.desktop
-
 ./start-tor-browser.desktop --register-app 
