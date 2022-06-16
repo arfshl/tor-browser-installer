cefc6d03e96392950dbf6732f8bf2627d9a6fb50
arfshl <98870352+arfshl@users.noreply.github.com>
2022-06-16 16:45:23 +0700

    Update install-su.sh

--- script/install-su.sh
+++ script/install-su.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+print "Tor browser installer"
+print "mv tor-browser-linux*.tar.bz2 /opt
+cd /opt
+tar xvjf tor-browser-linux*.tar.bz2
+rm tor-browser-linux*.tar.bz2
+cd /opt/tor-browser-linux*
+chmod +x start-tor-browser.desktop
+./start-tor-browser.desktop --register-app"
+read -p "Press any key to execute script ..."
 mv tor-browser-linux*.tar.bz2 /opt
 cd /opt
 tar xvjf tor-browser-linux*.tar.bz2
