c1c9dfbd7e6e6ea91846d132a2a6aa9c07ab467d
arfshl <98870352+arfshl@users.noreply.github.com>
2022-07-11 09:22:32 +0700

    Update install-su.sh

--- install-su.sh
+++ install-su.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Tor browser installer"
-wget https://www.torproject.org/dist/torbrowser/11.0.14/tor-browser-linux64-11.0.14_en-US.tar.xz -P /opt
+wget https://www.torproject.org/dist/torbrowser/11.0.15/tor-browser-linux64-11.0.15_en-US.tar.xz -P /opt
 cd /opt
 tar xvf tor-browser-linux*.tar.xz
 rm tor-browser-linux*.tar.xz
