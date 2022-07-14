fd040c973f137cfd53f93d948ac6670094fb3998
arfshl <98870352+arfshl@users.noreply.github.com>
2022-07-14 18:42:11 +0700

    Delete install-su.sh

--- install-su.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo "Tor browser installer"
-wget https://www.torproject.org/dist/torbrowser/11.0.15/tor-browser-linux64-11.0.15_en-US.tar.xz -P /opt
-cd /opt
-tar xvf tor-browser-linux*.tar.xz
-rm tor-browser-linux*.tar.xz
-cd /opt/tor-browser-linux*
-chmod +x start-tor-browser.desktop
-./start-tor-browser.desktop --register-app
