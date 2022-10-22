5d86a5162cb928e4ccf5f8db2c081c05cb8fddf9
arfshl <98870352+arfshl@users.noreply.github.com>
2022-10-22 18:56:20 +0700

    Create install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "Tor browser installer"
+echo "Installing Tor Browser..."
 sudo wget https://www.torproject.org/dist/torbrowser/11.5.4/tor-browser-linux64-11.5.4_en-US.tar.xz -P /opt
 sudo cd /opt
 sudo tar xvf tor-browser-linux*.tar.xz
