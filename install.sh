49ab907c9a838c8ebf7cd46b7f73946b4e7b58cd
arfshl <98870352+arfshl@users.noreply.github.com>
2023-05-24 00:15:19 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 echo "Installing Tor Browser..."
-cd ~/
-wget https://www.torproject.org/dist/torbrowser/12.0.6/tor-browser-linux64-12.0.6_ALL.tar.xz -P ~/
-tar xvf tor-browser-linux*.tar.xz
-chmod -R tor-browser*
-cd tor-browser*
-sudo chmod +x start-tor-browser.desktop
+wget https://www.torproject.org/dist/torbrowser/12.0.6/tor-browser-linux64-12.0.6_ALL.tar.xz -P /opt
+tar xvf /opt/tor-browser-linux*.tar.xz
+chmod -R /opt/tor-browser*
+sudo chmod +x /opt/tor-browser/start-tor-browser.desktop
+cd /opt/tor-browser*
 ./start-tor-browser.desktop --register-app
-cd ~/
-rm install.sh tor-browser-linux*.tar.xz
+rm ~/install.sh /opt/tor-browser-linux*.tar.xz
