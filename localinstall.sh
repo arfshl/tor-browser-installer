940dfde8ca1950c02692bcf003e2053a4a0fcc1c
arfshl <98870352+arfshl@users.noreply.github.com>
2024-06-10 02:38:28 +0000

    Add single-user install method

---
+++ localinstall.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo "Installing Tor Browser..."
+cd ~
+mkdir tor-browser
+wget https://www.torproject.org/dist/torbrowser/13.0.15/tor-browser-linux-x86_64-13.0.15.tar.xz -P ~/tor-browser
+tar xvf ~/tor-browser/tor-browser-linux*.tar.xz
+chmod -R ~/tor-browser/tor-browser*
+chmod +x ~/tor-browser/tor-browser/start-tor-browser.desktop
+cd ~/tor-browser/tor-browser*
+./start-tor-browser.desktop --register-app
+rm ~/install.sh ~/tor-browser/tor-browser-linux*.tar.xz
