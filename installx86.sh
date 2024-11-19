863ac9beca8a4b6672496e476ef313458d7de40e
Alif <98870352+arfshl@users.noreply.github.com>
2024-11-19 11:28:24 +0700

    Update installx86.sh

--- installx86.sh
+++ installx86.sh
@@ -6,4 +6,4 @@ chmod -R /opt/tor-browser*
 sudo chmod +x /opt/tor-browser/start-tor-browser.desktop
 cd /opt/tor-browser*
 sudo ./start-tor-browser.desktop --register-app
-rm ~/install.sh /opt/tor-browser-linux*.tar.xz
+rm ~/installx86.sh /opt/tor-browser-linux*.tar.xz
