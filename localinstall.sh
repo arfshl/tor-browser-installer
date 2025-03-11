6736573e2b5bf0ca72ddf5eb95ebd66c6fad1619
arfshl <98870352+arfshl@users.noreply.github.com>
2025-03-11 21:12:26 +0700

    Update localinstall.sh

--- localinstall.sh
+++ localinstall.sh
@@ -2,7 +2,7 @@
 echo "Installing Tor Browser..."
 cd ~
 mkdir tor-browser
-wget https://www.torproject.org/dist/torbrowser/14.0.2/tor-browser-linux-x86_64-14.0.2.tar.xz -P ~/tor-browser
+wget https://www.torproject.org/dist/torbrowser/14.0.7/tor-browser-linux-x86_64-14.0.7.tar.xz -P ~/tor-browser
 tar xvf ~/tor-browser/tor-browser-linux*.tar.xz
 chmod -R ~/tor-browser/tor-browser*
 chmod +x ~/tor-browser/tor-browser/start-tor-browser.desktop
